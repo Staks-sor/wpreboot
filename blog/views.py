16288189d912bb44eee0107d8a3e3c63e86e4171
Shahzod <shahzod20030905@gmail.com>
2022-09-11 15:04:47 +0200

    добавил исключения

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 import os
+from urllib.error import URLError, HTTPError
 
 from django.core.files.base import ContentFile, File
 from django.core.files.temp import NamedTemporaryFile
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
 from randomfilestorage.storage import RandomFileSystemStorage
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
+from random import choice
 from blog.models import Post
 import urllib.request as urllib2
 
@@ -57,19 +60,32 @@ class YandexTurbo(ListView):
 random_storage = RandomFileSystemStorage(location='/media/')
 
 class ParceObjects(APIView):
-    
+    image = []
+
     def post(self, request):
+
         try:
+            if len(self.image) >= 10:
+                self.image.remove(self.image[0])
+
             form = request.data
             title = form['title']
+            self.image.append(form['image'])
             img_temp = NamedTemporaryFile()
             img_temp.write(urllib2.urlopen(form['image']).read())
             img_temp.flush()
             Post.objects.create(title=form['title'], content=form['content'], image=File(img_temp, name=f'{title}.jpg'))
             return Response('', status=status.HTTP_201_CREATED)
-        except (ValueError, OSError, ):
+        except (ValueError, OSError,URLError, HTTPError):
+            form['image'] = choice(self.image)
             form = request.data
-            Post.objects.create(title=form['title'], content=form['content'],)
+            title = form['title']
+            img_temp = NamedTemporaryFile()
+            img_temp.write(urllib2.urlopen(form['image']).read())
+            img_temp.flush()
+            Post.objects.create(title=form['title'], content=form['content'],image=File(img_temp, name=f'{title}.jpg'))
             return Response('', status=status.HTTP_201_CREATED)
+        except:
+            return Response('Invalid data ', status=status.HTTP_200_OK)
 
 
